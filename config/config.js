5aec61a78e605dc9face1e8d1ea949cbe57f69bb
sajad1993gh <sajad1993gh@gmail.com>
2019-07-14 16:45:41 -0400

    setting up the connection config for heroku deployment

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+module.exports = {
+    "development": {
+        "username": "root",
+        "password": process.env.KEY,
+        "database": "burgers_db",
+        "host": "127.0.0.1",
+        "dialect": "mysql",
+        "operatorsAliases": false
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql",
+        "operatorsAliases": false
+    },
+    "production": {
+        "use_env_variable": "JAWSDB_URL",
+        "dialect": "mysql",
+    }
+}
